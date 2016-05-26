43f7b3b418141f1728d045e4576f95f5d01cd8fb
ysriram1 <Sriram@loop-depaulsecure-230-8.depaulsecure-student.depaul.edu>
2016-05-25 20:25:11 -0500

    improved optimization function

--- optimization.py
+++ optimization.py
@@ -6,10 +6,9 @@ This is a temporary script file.
 """
 import os
 import numpy as np
-import pandas as pd
-from scipy.optimize import minimize 
+from scipy.optimize import minimize, fmin_cobyla, linprog, fmin_cg
 
-os.chdir('C:/Users/SYARLAG1/Desktop/LIDC')
+os.chdir('/Users/Sriram/Desktop/DePaul/Work/LIDC')
 
 data = np.genfromtxt('./LIDC_REU2015.csv', delimiter = ',', skip_header = 1 , usecols = (range(11,76)))
 targets = np.genfromtxt('./LIDC_REU2015.csv', delimiter = ',', skip_header = 1, usecols = (84,93,102,111)).astype(int)
@@ -34,6 +33,40 @@ def dataPreprocess(data = data, targetArray = targets):
 
 X,S,D = dataPreprocess(data = data, targetArray = targets)
 
+
+def diagA_Xing(X,S,D):
+    #X: the data matrix
+    #S: the similarity Matrix
+    #D: the dissimilarty Matrix    
+    
+    #Creating the objective function and the constraints function here
+    def objective(A):
+        for i in range(S.shape[0]):
+            for j in range(S.shape[1]):
+                if S[i,j] == 0:
+                    continue
+                M = np.matrix(X[i] - X[j])
+                return np.sum(np.dot(np.dot(M, A), M))
+
+    def constraint(A):
+        for i in range(D.shape[0]):
+            for j in range(D.shape[1]):
+                if D[i,j] == 0:
+                    continue
+                M = np.matrix(X[i]-X[j])
+                value = np.sqrt(np.sum(np.dot(np.dot(M, A), M)))-1
+                if value == np.nan:
+                    return -1
+                else:
+                    return value
+    
+    A0 = np.random.rand(X.shape[1],1)
+    
+    return fmin_cobyla(objective, A0,[constraint],maxfun=10**10)
+
+result = diagA_Xing(X, S, D)
+
+
 def fullA_Xing(X,S,D):
     #X: the data matrix
     #S: the similarity Matrix
@@ -45,19 +78,23 @@ def fullA_Xing(X,S,D):
             for j in range(S.shape[1]):
                 if S[i,j] == 0:
                     continue
-                M = X[i] - X[j] 
-                return np.dot(np.dot(M.T,A),M)
-    
+                M = np.matrix(X[i] - X[j])
+                return np.sum(np.dot(np.dot(M, A), M))
+
     def constraint(A):
         for i in range(D.shape[0]):
             for j in range(D.shape[1]):
                 if D[i,j] == 0:
                     continue
-                M = X[i]-X[j]
-                return np.dot(np.dot(M.T, A), M)
-    
+                M = np.matrix(X[i]-X[j])
+                value = np.sqrt(np.sum(np.dot(np.dot(M, A), M.T)))-1
+                if value == np.nan:
+                    return -1
+                else:
+                    return value
+                    
     A0 = np.random.rand(X.shape[1], X.shape[1])
     
-    return minimize(objective, A0, method='COBYLA', constraints={'type':'ineq', 'fun':constraint})
+    return fmin_cobyla(objective, A0, [constraint], rhoend=1e-7)
 
 result = fullA_Xing(X, S, D)
